c9651bd6939dba4166de64d229e9e9a7c43af539
Cleferson Dannylo <clefersonnascimento8@gmail.com>
2021-12-07 23:06:03 -0200

    gerando um html estático

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
@@ -9,6 +11,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [{ test: /\.jsx$/, exclude: /node_modules/, use: 'babel-loader' }]
   }
